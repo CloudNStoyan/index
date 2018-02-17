fb80eda8f4f9632647e7d0db85cc1dd3540ffae5
Stoyan <slownessbg@gmail.com>
2018-02-17 17:34:42 +0200

    Fixing main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("input");
 
-btn.addEventListener(function () {
+btn.addEventListener('click',function () {
    console.log(input.value)
 });
