e2c1f0c730614d5912d9c4049e872c5096c8a62d
Stoyan <slownessbg@gmail.com>
2018-02-24 23:07:14 +0200

    Adding new feature

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const nameSearchBtn = document.getElementById("name-submit");
 const input = document.getElementById("name-input");
-const dropMenuBtn = document.getElementsByClassName("dropbtn");
+const profileBox = document.getElementById('profile-information');
+const currentSoloQueueRank = document.getElementById('solo-queue');
+const currentFlexQueueRank = document.getElementById('flex-queue');
 
-nameSearchBtn.addEventListener('click',function () {
-   console.log(input.value);
-    input.value = " ";
+
+nameSearchBtn.addEventListener('click',function (e) {
+    e.preventDefault();
+    if (input.value !== " ") {   console.log(input.value); }
+    input.value = "";
 });
 
 
+currentSoloQueueRank.addEventListener('mouseover',function () {
+    console.log('Wow');
+    currentFlexQueueRank.style.display = 'block';
+});
